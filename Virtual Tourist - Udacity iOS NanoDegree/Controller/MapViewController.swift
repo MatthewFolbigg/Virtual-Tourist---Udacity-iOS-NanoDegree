7d251a3da834a87ace6bce531c7f45c1dbdf8286
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-03-01 11:06:08 +0000

    Added initial view controllers

---
+++ Virtual Tourist - Udacity iOS NanoDegree/Controller/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Virtual Tourist - Udacity iOS NanoDegree
+//
+//  Created by Matthew Folbigg on 01/03/2021.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+
+}
+
---
+++ Virtual Tourist - Udacity iOS NanoDegree/Controller/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Virtual Tourist - Udacity iOS NanoDegree
+//
+//  Created by Matthew Folbigg on 01/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    
+    
+}
+
