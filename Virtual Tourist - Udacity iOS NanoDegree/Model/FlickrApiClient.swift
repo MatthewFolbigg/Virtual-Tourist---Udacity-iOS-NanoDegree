ba337b6bf16a9cd0567847802e4e444b36b7283b
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-03-04 11:14:33 +0000

    Started work on flickrAPI to get photos for location.

---
+++ Virtual Tourist - Udacity iOS NanoDegree/Model/FlickrApiClient.swift
@@ -0,0 +1,21 @@
+//
+//  FlickrApiClient.swift
+//  Virtual Tourist - Udacity iOS NanoDegree
+//
+//  Created by Matthew Folbigg on 04/03/2021.
+//
+
+import Foundation
+
+class FlickrApiClient {
+    
+    var baseUrlString = "https://api.flickr.com/services"
+    var apiKey = "a74edbd67a37cc3efecda3a4f694f145"
+    
+    enum Endpoints {
+        case getPhotosForLocation
+        
+        
+    }
+    
+}
