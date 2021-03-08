06de89afbbdbfeb04649932dbd12fc187e47516a
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-03-08 12:35:31 +0000

    Added comments

--- Virtual Tourist - Udacity iOS NanoDegree/Model/Flickr API/FlickrApiPrivate.swift
+++ Virtual Tourist - Udacity iOS NanoDegree/Model/Flickr API/FlickrApiPrivate.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 struct flickrPrivate {
+    //Store API related keys seperate from main code
     static var shared = flickrPrivate()
     var apiKey = "a74edbd67a37cc3efecda3a4f694f145"
 }
